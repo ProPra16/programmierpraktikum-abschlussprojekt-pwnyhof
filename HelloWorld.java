3e92af6672f0c54bb7aa3f1e65d4cbb1a7cdb161
ObiHanKenobi <asiapride@hotmail.com>
2016-06-18 15:16:20 +0200

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld{
 	
 	public static void main(String[] args){
 		System.out.println("Guten Tag die Herren!");
+		System.out.println("Hello Moto :) yannick und johannes muessen mir mal followen damit 
+		ich euch zurueck followen kann da! :D aber yay es geht..hab alles gemerged von chris. 
+		kP was ich da nun alles habe");
 	}
 }
