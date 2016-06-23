2698a09549674ad4d8ab36793922b005a486fb0a
Chris <chris.eder96@googlemail.com>
2016-06-23 20:16:39 +0200

    Gedankenexperiment

--- src/main/TDDTMain.java
+++ src/main/TDDTMain.java
@@ -2,6 +2,7 @@ package main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import layout.window.MainWindow;
@@ -16,7 +17,17 @@ public class TDDTMain extends Application{
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("TDDT");
+		
 		GridPane root = new GridPane();
+		
+	    ColumnConstraints column1 = new ColumnConstraints();
+	    column1.setPercentWidth(50);
+	    ColumnConstraints column2 = new ColumnConstraints();
+	    column2.setPercentWidth(50);
+	    root.getColumnConstraints().addAll(column1, column2);
+	     
+        root.setHgap(10);
+        root.setVgap(10);
 		
 		MainWindow.buildWindow(root);
     
