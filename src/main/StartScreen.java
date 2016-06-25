29f56390aea889ab861f8cf3f891d313e5610c73
ObiHanKenobi <asiapride@hotmail.com>
2016-06-25 17:47:29 +0200

    u

--- src/main/StartScreen.java
+++ src/main/StartScreen.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,16 +9,14 @@ import javafx.stage.Stage;
 public class StartScreen extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			AnchorPane page = (AnchorPane) FXMLLoader.load(StartScreen.class.getResource("StartLayout.fxml"));
-			Scene scene = new Scene(page);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("TDDT 1.0");
-			primaryStage.show();
-		} catch (Exception ex) {
-			Logger.getLogger(StartScreen.class.getName()).log(Level.SEVERE, null, ex);
-		}
+	public void start(Stage primaryStage) throws Exception {
+
+		AnchorPane page = (AnchorPane) FXMLLoader.load(StartScreen.class.getResource("StartLayout.fxml"));
+		Scene scene = new Scene(page);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("TDDT 1.0");
+		primaryStage.show();
+
 	}
 
 }
