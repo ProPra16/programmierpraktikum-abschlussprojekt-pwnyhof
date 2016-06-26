535cb7e3b6147d145d0469bf0f6df2640f3e9f6b
JakeReeco <yannick.scheuer@uni-duesseldorf.de>
2016-06-26 21:29:47 +0200

    Wo auch immer die StartLayout.fxml is, ich find die nicht xD

--- src/main/StartScreen.java
+++ src/main/StartScreen.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package main;
 
 import java.util.logging.Level;
@@ -14,38 +14,12 @@ public class StartScreen extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		AnchorPane page = (AnchorPane) FXMLLoader.load(StartScreen.class.getResource("StartLayout.fxml"));
+			//Wo ist denn die StartLayout.fxml ?
+		AnchorPane page = FXMLLoader.load(StartScreen.class.getResource("StartLayout.fxml"));
 		Scene scene = new Scene(page);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("TDDT 1.0");
 		primaryStage.show();
 	}
-
+	public static void main(String[] args){ launch();}
 }
-=======
-package main;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-public class StartScreen extends Application {
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-
-		AnchorPane page = (AnchorPane) FXMLLoader.load(StartScreen.class.getResource("StartLayout.fxml"));
-		Scene scene = new Scene(page);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("TDDT 1.0");
-		primaryStage.show();
-	}
-
-}
->>>>>>> refs/remotes/origin/Han
