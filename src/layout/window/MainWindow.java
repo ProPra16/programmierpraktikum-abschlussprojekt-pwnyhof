5e695497279a5144eb2180b2ffce9e8001008bf7
ObiHanKenobi <asiapride@hotmail.com>
2016-06-26 17:37:55 +0200

    vorlaeufiger entwurf
    also startbildschirm denke ich kann man so lassen.
    der Editorscreen ist bereits voll beschreibbar. man musste noch das
    linke code-textfeld locken fuer den ersten schritt. ihr koennt ja mal
    drueberschauen und mir sagen was ihr davon haltet. ich habe noch ein
    kleines problem:
    
    Und zwar weiss ich nicht wie ich es schaffe dass die groesse der Panes
    und Areas in meinem screen sich automatisch vergroessert wenn man das
    Fenster groesser macht. vllt habt ihr eine Loesung dafuer. Desweeiteren
    ist mein Code so wie er jezz is etwas ueberladen. Vllt kann einer von
    euch das in objekte aufteilen, sodass der code kleiner wird.

--- src/layout/window/MainWindow.java
+++ src/layout/window/MainWindow.java
@@ -4,16 +4,14 @@ import javafx.scene.layout.GridPane;
 import layout.tools.Bar;
 import layout.tools.Red;
 
-public class MainWindow{
+public class MainWindow {
 
-	public static void buildWindow(GridPane root){
+	public static void buildWindow(GridPane root) {
 		Red red = new Red();
 		root.add(red.getTool(), 0, 1);
-		
-		
+
 		Bar menubar = new Bar();
 		root.add(menubar.getTool(), 0, 0, 2, 1);
 	}
-	
 
 }
