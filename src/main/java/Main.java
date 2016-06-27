f2d61badc2dcd906cd13a8fc73657acd65388e9f
Chris <chris.eder96@googlemail.com>
2016-06-27 21:46:07 +0200

    Umordnung

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args){
-		Library lib = new Library();
-		System.out.println(lib.someLibraryMethod());
-	}
-}
---
+++ src/main/java/TDDTrainer.java
@@ -0,0 +1,11 @@
+import main.TDDTMain;
+
+public class TDDTrainer{
+
+	//Programmstart
+	//Test
+	
+	public static void main(String[] args){
+		TDDTMain.start(args);
+	}
+}
