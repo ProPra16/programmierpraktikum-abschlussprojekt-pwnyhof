c49de5e9710c2205803270c0bfe0e4f0414eacdd
Chris <chris.eder96@googlemail.com>
2016-06-28 13:05:32 +0200

    Neueinrichtung

---
+++ src/main/java/TDDTStart.java
@@ -0,0 +1,8 @@
+import main.TDDTMain;
+
+public class TDDTStart {
+
+	public static void main(String[] args){
+		TDDTMain.start(args);
+	}
+}
---
+++ src/main/java/layout/tools/Tool.java
@@ -0,0 +1,9 @@
+package layout.tools;
+
+import javafx.scene.Node;
+
+public interface Tool {
+
+	public Node getTool();
+
+}
