4ecf7e2af1a254f66ae7ec2a94610c91d601e91a
Chris <chris.eder96@googlemail.com>
2016-07-02 16:05:00 +0200

    Umstellung auf FXML

--- src/main/java/TDDTStart.java
+++ src/main/java/TDDTStart.java
@@ -1,4 +1,4 @@
-package de.hhu.propra16.tddt;
+//package de.hhu.propra16.tddt;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,14 +11,19 @@ public class TDDTStart extends Application {
 
 	public void start(Stage stage) throws Exception {
 
+		try{
 		// Load root layout from fxml file.
-		BorderPane base = FXMLLoader.load(getClass().getResource("/layout/tools/TextScreen.fxml"));
+		BorderPane base = FXMLLoader.load(getClass().getResource("./de/hhu/propra16/tddt/layout/TextScreen.fxml"));
 
 		// Show the scene containing the root layout.
 		Scene scene = new Scene(base);
 		Stage primaryStage = new Stage();
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		}
+		catch(Exception e){
+			
+		}
 
 	}
 
