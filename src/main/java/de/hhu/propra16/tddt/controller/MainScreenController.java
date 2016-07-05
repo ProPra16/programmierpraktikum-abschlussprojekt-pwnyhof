45c5127398e7c12c632d4ba4e2086234c35008f0
ObiHanKenobi <asiapride@hotmail.com>
2016-07-05 23:34:09 +0200

    update

--- src/main/java/de/hhu/propra16/tddt/controller/MainScreenController.java
+++ src/main/java/de/hhu/propra16/tddt/controller/MainScreenController.java
@@ -9,6 +9,10 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.smartcardio.CommandAPDU;
+
+import de.hhu.propra16.tddt.TDDTStart;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +20,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
@@ -24,6 +29,7 @@ import javafx.stage.Stage;
 public class MainScreenController {
 	Runtime rt = Runtime.getRuntime();
 	private Stage stage;
+	String commandLine = " ";
 
 	@FXML
 	public MenuItem neu;
@@ -53,6 +59,9 @@ public class MainScreenController {
 	public Button runCommand;
 
 	@FXML
+	public Button runWithCommand;
+
+	@FXML
 	public Button runCode;
 
 	@FXML
@@ -63,6 +72,9 @@ public class MainScreenController {
 
 	@FXML
 	public TextArea rightTA;
+
+	@FXML
+	public TextField commandField;
 
 	@FXML
 	public TextArea console;
@@ -109,7 +121,7 @@ public class MainScreenController {
 				}
 				bufferedLoad.close();
 			} catch (IOException ex) {
-				// System.err.println(ex);
+
 			}
 
 		}
@@ -134,7 +146,7 @@ public class MainScreenController {
 				}
 				bufferedLoad.close();
 			} catch (IOException ex) {
-				// System.err.println(ex);
+
 			}
 		}
 		if (e.getSource() == saveTest) {
@@ -192,7 +204,7 @@ public class MainScreenController {
 
 			program.compile();
 
-			program.run();
+			program.run(commandLine);
 
 		}
 		if (e.getSource() == runTest) {
@@ -216,20 +228,35 @@ public class MainScreenController {
 			console.clear();
 		}
 		if (e.getSource() == runCommand) {
+
 			try {
 				// Load root layout from fxml file.
-				AnchorPane command = FXMLLoader.load(getClass().getResource("./controller/CommandLineScreen.fxml"));
+				AnchorPane command = FXMLLoader.load(getClass().getResource("CommandLineScreen.fxml"));
 
 				// Show the scene containing the root layout.
-
-				Stage commandStage = new Stage();
 				Scene scene = new Scene(command);
+				Stage commandStage = new Stage();
 				commandStage.setScene(scene);
+
 				commandStage.show();
 
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
+		}
+		if (e.getSource() == runWithCommand) {
+			ConfigReader config = new ConfigReader("Aufgabe1");
+
+			Information info = new Information(config.getTestName(), config.getProgramName(),
+					"./Task/" + config.getTask() + "/");
+
+			Program program = new Program(info, console);
+			commandLine = " " + commandField.getText();
+			System.out.print(commandLine);
+			program.compile();
+
+			
+			program.run(commandLine);
 		}
 
 	}
