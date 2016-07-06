80c275b73d4a748b6d1230d3b1db67872f2e682d
Chris <chris.eder96@googlemail.com>
2016-07-06 12:08:29 +0200

    Update Try.java

--- Task/Aufgabe1/Try.java
+++ Task/Aufgabe1/Try.java
@@ -14,10 +14,7 @@ public class Try{
 	
 	@Test
 	public void addTest(){
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/Propra16/programmierpraktikum-abschlussprojekt-pwnyhof
 		assertEquals(9, Code.add());
 	}
 }
