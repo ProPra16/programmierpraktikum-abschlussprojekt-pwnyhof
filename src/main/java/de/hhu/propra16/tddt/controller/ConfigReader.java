7de33b9620926da0b97475789a38c88cba412752
Yannick Scheuer <yannick.scheuer@uni-duesseldorf.de>
2016-07-14 13:13:23 +0200

    some javadoc for configreader
    tada

--- src/main/java/de/hhu/propra16/tddt/controller/ConfigReader.java
+++ src/main/java/de/hhu/propra16/tddt/controller/ConfigReader.java
@@ -5,12 +5,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
+/**
+ * 
+ * @author group pwnyhof
+ *
+ */
 public class ConfigReader {
 	private String task;
 	private List<String> contentOfConfig;
 	private String filePathToTask;
-	
+	/**
+	 * 
+	 * @param task is a string type variable which contains the name of the folder in which
+	 * your config.txt should exist. If it's not there you will get an Error.
+	 */
 	public ConfigReader(String task){
 		this.task = task;
 		filePathToTask = "./Task/" + task + "/";
@@ -23,15 +31,24 @@ public class ConfigReader {
 			System.out.println("ERROR: Config.txt Not Found!");
 		}
 	}
-	
+	/**
+	 * getter to
+	 * @return the actual task
+	 */
 	public String getTask(){
 		return task;
 	}
-	
+	/**
+	 * getter to
+	 * @return the file path from the task
+	 */
 	public String getPath(){
 		return filePathToTask;
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getProgramName(){
 		String lineWithInfo = contentOfConfig.get(0);
 		String programName = lineWithInfo.substring(13);
