a375a73f34a995588f44ddccf2b16e95d8892108
Yannick Scheuer <yannick.scheuer@uni-duesseldorf.de>
2016-07-15 05:42:53 +0200

    Comment to ConfigReaderTest, removed TrackingTest

--- src/test/java/ConfigReaderTest.java
+++ src/test/java/ConfigReaderTest.java
@@ -4,7 +4,14 @@ import org.junit.Test;
 
 import de.hhu.propra16.tddt.controller.ConfigReader;
 import de.hhu.propra16.tddt.controller.MainScreenController;
-
+/*
+ * Der Test hier war ein gescheiterter Versuch das bestehende
+ * System darauf zu testen, was geschieht, wenn eine falsche
+ * Directory angegeben wird. Hat aber nicht geklappt, da die
+ * Methoden aus ConfigReader alle ohne Uebergabeparameter
+ * aufgerufen werden und daher nicht mit beliebigen Pfaden
+ * angesteuert werden koennen.
+ */
 public class ConfigReaderTest {
 
 	@Test
